bdc1f18e354362e27edc6fb22b921bd7357e71cc
Anderson Domingues <ti.andersondomingues@gmail.com>
2022-08-07 22:47:37 -0300

    update hibernate config builder

--- src/main/java/com/warehouse/HibernateHelper.java
+++ src/main/java/com/warehouse/HibernateHelper.java
@@ -5,25 +5,55 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.fasterxml.jackson.databind.deser.impl.NullsFailProvider;
 
 public class HibernateHelper {
 
 	private static SessionFactory sessionFactory = null;
-	
-	private static SessionFactory buildSessionFactory() 
+
+	private static SessionFactory buildSessionFactory()
 	{
-		try 
+		try
 		{
-			if (sessionFactory == null) 
+			if (sessionFactory == null)
 			{
+				// System.out.println("hibernate_db_host: " + System.getProperty("hibernate_db_host"));
+				// System.out.println("hibernate_db_name: " + System.getProperty("hibernate_db_name"));
+				// System.out.println("hibernate_db_user: " + System.getProperty("hibernate_db_user"));
+				// System.out.println("hibernate_db_pass: " + System.getProperty("hibernate_db_pass"));
+
+				StringBuilder builder = new StringBuilder();
+				builder.append("jdbc:postgresql://")
+					.append(System.getProperty("hibernate_db_host"))
+					.append("/")
+					.append(System.getProperty("hibernate_db_name"));
+
+				Configuration cfg = new Configuration();
+				cfg.setProperty("hibernate.connection.url", builder.toString());
+				
+				cfg.setProperty("hibernate.connection.username", 
+					System.getProperty("hibernate_db_user"));
+				
+				cfg.setProperty("hibernate.connection.password", 
+					System.getProperty("hibernate_db_pass"));
+
 				StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
 					//.configure("hibernate.cfg.xml").build();
-					.configure().build();
-				
+					// .configure().build();
+					.configure()
+					.applySettings(cfg.getProperties())
+					.build();
+
 				Metadata metaData = new MetadataSources(standardRegistry)
 						.getMetadataBuilder()
 						.build();
 				
+				
+									
+				// sessionFactory = cfg.buildSessionFactory();
+
 				sessionFactory = metaData.getSessionFactoryBuilder().build();
 			}
 			return sessionFactory;
